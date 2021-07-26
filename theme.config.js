b1ed09ce833b88d6f6b885b15ee664cc93111f57
Jarrett Azar <30982357+JarrettAzar@users.noreply.github.com>
2021-07-26 16:25:30 -0400

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,23 +1,6 @@
 const YEAR = new Date().getFullYear()
 
 export default {
-  header: (
-    <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Jarrett Azar.
-      <a href="/feed.xml">RSS</a>
-      <style jsx>{`
-        a {
-          float: right;
-        }
-        @media screen and (max-width: 480px) {
-          article {
-            padding-top: 2rem;
-            padding-bottom: 4rem;
-          }
-        }
-      `}</style>
-    </small>
-  )
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © Jarrett Azar.
